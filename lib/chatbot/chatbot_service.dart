c85a5b6062dafa0d2b721fb6a5cbcf2ea2304f8c
joven <jaybenrosales12@gmail.com>
2025-05-07 01:07:16 +0800

    Added Chatbot, Added animation

--- lib/chatbot/chatbot_service.dart
+++ lib/chatbot/chatbot_service.dart
@@ -6,8 +6,7 @@ class ChatbotService {
   final String apiKey = dotenv.env['GEMINI_API_KEY'] ?? '';
 
   Future<String> sendMessage(String message) async {
-    const apiUrl =
-        'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent';
+    const apiUrl ='https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent';
 
     final response = await http.post(
       Uri.parse('$apiUrl?key=$apiKey'),
