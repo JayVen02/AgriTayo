c303853ada84a8a1e9e8359dbe38242583583085
Girlyver C. Jandayan <girlyverjandayan@gmail.com>
2025-05-10 10:50:53 +0800

    UI of user profile

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'marketplace_screen.dart';
+import 'marketplace_screen.dart'; //import of marketplace_screen.dart
 
 
 class ProfileScreen extends StatelessWidget {
